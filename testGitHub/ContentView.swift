2752dea291db39db4bebc04fdcdd37e42ca51587
jeremykjackson <jeremykjackson@me.com>
2021-11-13 22:08:23 -0600

    Initial Commit

---
+++ testGitHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testGitHub
+//
+//  Created by Jeremy Jackson on 11/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testGitHub/testGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  testGitHubApp.swift
+//  testGitHub
+//
+//  Created by Jeremy Jackson on 11/13/21.
+//
+
+import SwiftUI
+
+@main
+struct testGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
